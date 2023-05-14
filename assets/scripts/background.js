5d8ebadee4eeefdffe2901691b6efb6e01bc092b
unknown <bkreponte@ucsd.edu>
2023-05-14 07:37:39 -0700

    Added randomized face up cards in background

--- assets/scripts/background.js
+++ assets/scripts/background.js
@@ -8,7 +8,7 @@ function init() {
     }
 
     /* creates 25 cards */
-    for (let i = 0; i < 25; i++) {
+    for (let i = 0; i < 52; i++) {
         createCard();
     }
 }
@@ -17,9 +17,24 @@ function init() {
  * TODO Create a background card with random rotation and position
  */
 function createCard() {
+    const main = document.querySelector('main');
     const card = document.createElement('img');
-    const randomFace = null;
+    
+    const face = Math.floor(Math.random() * (52 - 1) + 1);
+    const randomFace = `assets/images/cards/${face}.png`;
     card.setAttribute('src', randomFace);
+
+    const positionX = Math.floor(Math.random() * 100);
+    const positionY = Math.floor(Math.random() * 85);
+    const rotation = Math.floor(Math.random() * 180);
+    card.style.position = 'absolute';
+    card.style.transform = `rotate(${rotation}deg)`;
+    card.style.width = '1.875in';
+    card.style.height = '2.625in';
+    card.style.top = `${positionY}%`;
+    card.style.left = `${positionX}%`;
+
+    main.appendChild(card);
 }
 
 /*
